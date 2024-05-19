4e330146af26767829bb11b60a22bf578aa10bd0
vic4or <a20142931@pucp.edu.pe>
2024-05-18 22:19:47 -0500

    cambios

---
+++ backend/logisticaReparto/src/main/java/com/redex/logisticaReparto/model/AeropuertoModel.java
@@ -0,0 +1,10 @@
+package com.redex.logisticaReparto.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Aeropuerto")
+public class AeropuertoModel {
+
+}
