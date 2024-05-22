aaf0e06dc368830312bea3768727c8a8583ca3b1
Juan_Angelo <angelo.flores@pucp.edu.pe>
2024-05-22 17:22:42 -0500

    Ultimo

--- backend/logisticaReparto/src/main/java/com/redex/logisticaReparto/model/ElementoListaRestringida.java
+++ backend/logisticaReparto/src/main/java/com/redex/logisticaReparto/model/ElementoListaRestringida.java
@@ -1,4 +1,25 @@
 package com.redex.logisticaReparto.model;
 
-public class ListaRestringida {
+import lombok.Getter;
+import lombok.Setter;
+import java.util.ArrayList;
+
+@Getter
+@Setter
+public class ElementoListaRestringida {
+
+    private int id_elementoLista;
+    public ArrayList<Integer> listaElementos;
+    private double  fitnessSolucion;
+
+    public ElementoListaRestringida() {
+
+    }
+
+    public ElementoListaRestringida(ArrayList<Integer> listaElementos, double fitnessSolucion) {
+        this.listaElementos = listaElementos;
+        this.fitnessSolucion = fitnessSolucion;
+    }
+
+
 }
