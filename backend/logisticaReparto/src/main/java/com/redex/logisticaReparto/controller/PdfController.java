d2b2ff6e462d278b3b86ef6b588f80b45670e02a
Juan Angelo <angelo.flores@pucp.edu.pe>
2024-06-12 23:10:03 -0500

    PDF 2

--- backend/logisticaReparto/src/main/java/com/redex/logisticaReparto/controller/PdfController.java
+++ backend/logisticaReparto/src/main/java/com/redex/logisticaReparto/controller/PdfController.java
@@ -1,32 +1,38 @@
 package com.redex.logisticaReparto.controller;
+
 import com.redex.logisticaReparto.services.PdfService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
 @RestController
-@RequestMapping("api")
+@RequestMapping(path = "api")
 public class PdfController {
-
+    private final PdfService reporteSimulacionPDFService;
+    //private final VehiculoBDService vehiculoBDService;
     @Autowired
-    private PdfService pdfService;
+    public PdfController(PdfService reporteSimulacionPDFService
+                                          //,VehiculoBDService vehiculoBDService
+                                          ){
+        this.reporteSimulacionPDFService = reporteSimulacionPDFService;
+        //this.vehiculoBDService = vehiculoBDService;
+    }
 
-    @GetMapping("/generate")
-    public ResponseEntity<byte[]> generatePdf() {
-        try {
-            byte[] pdfBytes = pdfService.generatePdf();
-
-            HttpHeaders headers = new HttpHeaders();
-            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=documento.pdf");
-            headers.add(HttpHeaders.CONTENT_TYPE, "application/pdf");
-
-            return new ResponseEntity<>(pdfBytes, headers, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    @GetMapping(value = "/generar",produces =  MediaType.APPLICATION_PDF_VALUE)
+    public ModelAndView generarPDF(){
+        //List<VehiculoBD> vehiculos = vehiculoBDService.listar();
+        Map<String, Object> model = new HashMap<>();
+        //model.put("Vehiculos", vehiculos);
+        return new ModelAndView(reporteSimulacionPDFService,model);
     }
 }
